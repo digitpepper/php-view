c71561f5d2363542473fdad28c69485382e853f8
digitpepper <postmaster@digitpepper.com>
2023-03-29 16:01:21 +0800

    first commit

---
+++ src/View.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace DP;
+
+class View
+{
+	/**
+	 * @var bool
+	 */
+	public static $is_constructed = false;
+
+	/**
+	 * @var string
+	 */
+	public static $layout = 'layout';
+
+	/**
+	 * @var string
+	 */
+	public static $view_path = null;
+
+	public static function construct(): void
+	{
+		$app_path = '';
+		if (defined('APP_PATH')) {
+			$app_path = APP_PATH;
+		}
+		self::$view_path = $app_path . '/protected/views';
+		self::$is_constructed = true;
+	}
+
+	/**
+	 * @param string $view
+	 * @param array <string, mixed> $data
+	 * @param bool $return
+	 * @return string|false|void
+	 */
+	public static function render_partial(string $view, array $data = [], bool $return = false)
+	{
+		if (!self::$is_constructed) {
+			self::construct();
+		}
+		extract($data, EXTR_SKIP);
+		ob_start();
+		require self::$view_path . "/{$view}.php";
+		$content = ob_get_clean();
+		if ($return) {
+			return $content;
+		} else {
+			echo $content;
+		}
+	}
+
+	/**
+	 * @param string $view
+	 * @param array <string, mixed> $data
+	 * @param bool $return
+	 * @return string|false|void
+	 */
+	public static function render(string $view, array $data = [], bool $return = false)
+	{
+		$content = self::render_partial($view, $data, true);
+		$data['content'] = $content;
+		return self::render_partial(self::$layout, $data, $return);
+	}
+}
